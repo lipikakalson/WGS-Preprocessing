4c60a2247016adb4f898f67d0df32a62e64d2cda
lipika.kalson <84702870+lipikakalson@users.noreply.github.com>
2024-02-21 20:22:38 +0100

    Create trimmer.sh

---
+++ scripts/trimmer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name='b-trimmer'
+#SBATCH --mail-user=lipika.lipika@medunigraz.at
+#SBATCH --partition=cpu
+#SBATCH --error=error-btrimmer.e
+#SBATCH --output=output-btrimmer.o
+
+
+# Define the directory containing input files
+INPUT_DIR="/home/isilon/patho_anemone-meso/updated-data-bam/fastq"
+# Define the directory to save trimmed files
+OUTPUT_DIR="/home/isilon/patho_anemone-meso/updated-data-bam/fastq/trimmed"
+
+# Loop through all files in the directory
+for file1 in $INPUT_DIR/*.R1.fastq.gz; do
+  # Extract filename without path and directory part
+  # Replace _R1 with _R2 to get R2 filename using sed
+  filename1="${file1##*/}"
+  dir_part="${file1%/*}"
+  echo $file1
+  
+  # Replace _R1 with _R2 to get R2 filename
+  filename2=$(echo "$filename1" | sed 's/R1/R2/g')
+
+  r2_file="$dir_part/$filename2"
+  echo "$r2_file"
+
+  # Submit a job to trim the files
+  sbatch \
+    --job-name="trim_${filename1}" \
+    --output="output-trim_${filename1}.o" \
+    --error="error-trim_${filename1}.e" \
+    --partition=cpu \
+    --cpus-per-task=36 \
+    --wrap="java -jar /home/gpfs/o_lipika/PhD-analysis/agent/lib/trimmer-3.0.5.jar \
+    -fq1 \"$file1\" \
+    -fq2 \"$r2_file\" \
+    -v2 \
+    -out_loc \"$OUTPUT_DIR\""
+done
