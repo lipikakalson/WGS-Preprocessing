0ea53ed32c1f9a63a5be7dbb0377a26941ead368
lipika.kalson <84702870+lipikakalson@users.noreply.github.com>
2024-06-24 19:40:42 +0200

    Update mark-dup.sh

--- scripts/mark-dup.sh
+++ scripts/mark-dup.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name='mark-duplicates'
 #SBATCH --mail-user=lipika.lipika@medunigraz.at
 #SBATCH --partition=cpu
-#SBATCH --error=mark-duplicates.e
-#SBATCH --output=mark-duplicates.o
+#SBATCH --error=markdup/mark-duplicates.e
+#SBATCH --output=markdup/mark-duplicates.o
 
 # Define the directory containing input files
-INPUT_DIR="/home/isilon/patho_anemone-meso/updated-data-bam/fastq/trimmed"
+INPUT_DIR="/home/isilon/patho_anemone-meso/fastq/merged/batch2"
 # Define the output directory
-OUTPUT_DIR="/home/isilon/patho_anemone-meso/updated-data-bam/fastq/trimmed"
+OUTPUT_DIR="/home/isilon/patho_anemone-meso/fastq/dedup"
 TMP_FILE="/home/isilon/patho_anemone-meso/tmp"
 
 # Ensure picard.jar is in the PATH or specify its full path
@@ -17,26 +17,28 @@ PICARD_JAR="/home/gpfs/o_lipika/PhD-analysis/picard.jar"
 ulimit -c unlimited
 
 # Loop through all files in the directory
-for bam_file in "$INPUT_DIR"/*out_sorted_RG.bam; do
+for bam_file in "$INPUT_DIR"/*merged.bam; do
   echo "bam file: $bam_file"
   # Extract filename without path and directory part
   filename=$(basename "$bam_file")
   filename1="${bam_file%.bam}"
 
-  mkdir -p "$TMP_FILE/$filename"
-  tmp="$TMP_FILE/$filename"
+  #mkdir -p "$TMP_FILE/$filename"
+  #tmp="$TMP_FILE/$filename"
 
 
   
   sbatch <<EOF
 #!/bin/bash
 #SBATCH --job-name=markdup_${filename%.*}
-#SBATCH --output=markdup_${filename%.*}.out
-#SBATCH --error=markdup_${filename%.*}.err
+#SBATCH --output=markdup/markdup_${filename%.*}.out
+#SBATCH --error=markdup/markdup_${filename%.*}.err
 #SBATCH --partition=cpu
-#SBATCH --cpus-per-task=36
+#SBATCH --cpus-per-task=24
 
-java -jar "$PICARD_JAR" UmiAwareMarkDuplicatesWithMateCigar -I "${INPUT_DIR}/${filename}" -O "${filename1}_dedup.bam" -M "${filename1}_duplicate_metrics.txt" -UMI_METRICS "${filename1}_umi_metrics.txt" --BARCODE_TAG BC --TMP_DIR "$tmp" --REMOVE_DUPLICATES true
+java -Djava.io.tmpdir="$TEMPDIR" -Dsamjdk.threads=24 -jar "$PICARD_JAR" MarkDuplicates -I "${INPUT_DIR}/${filename}" -O "${filename1}_dedup.bam" -M "${filename1}_duplicate_metrics.txt" --BARCODE_TAG RX --TMP_DIR $TEMP
+
+#java -jar "$PICARD_JAR" MarkDuplicates -I /home/isilon/patho_anemone-meso/fastq/merged/batch1/D6039-9__D6039-9_S3_230904_A01664_0191_BHKW72DSX7_merged.bam -O /home/isilon/patho_anemone-meso/fastq/merged/batch1/D6039-9__D6039-9_S3_230904_A01664_0191_BHKW72DSX7_merged_dedup_markduplicates.bam  --BARCODE_TAG RX -M D6039-9_output_duplicate_metrics_md.txt  --TMP_DIR /home/isilon/patho_anemone-meso/tmp/D6039-9__tmp_md
 
 rm -r "$tmp"
 EOF
