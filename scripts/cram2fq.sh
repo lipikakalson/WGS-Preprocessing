fe242dde59bedcfc7af680e9bf355bb07579cd88
lipika.kalson <84702870+lipikakalson@users.noreply.github.com>
2024-06-24 19:31:58 +0200

    Update cram2fq.sh

--- scripts/cram2fq.sh
+++ scripts/cram2fq.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
 #SBATCH --job-name='cram2fq'
-#SBATCH --cpus-per-task=40
-#SBATCH --mem=512GB
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=256GB
 #SBATCH --mail-user=lipika.lipika@medunigraz.at
 #SBATCH --partition=cpu
-#SBATCH --cpus-per-task=24
-#SBATCH --error=error-cram2fq.e
-#SBATCH --output=output-cram2fq.o
+#SBATCH --error=cram2fq/error-cram2fq.e
+#SBATCH --output=cram2fq/output-cram2fq.o
 
 squeue -u 'o_lipika'
 
-reference="/home/isilon/users/o_lipika/FFPE-WGS-samples/BAM/data/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
-output_folder="fastq"
+input="/home/isilon/users/o_lipika/FFPE-WGS-samples/cram-files/batch2/"
+reference="/home/isilon/users/o_lipika/FFPE-WGS-samples/refernces/ref-karl/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+output_folder="/home/isilon/patho_anemone-meso/fastq/fq-files"
 
-mkdir -p "$output_folder"
-
-for cram_file in *.cram; do
+for cram_file in "$input"*.cram; do
     echo "Processing $cram_file"
     # Extract the sample name without the file extension
     sample_name=$(basename "$cram_file" .cram)
 
-    # Run samtools fastq for each CRAM file
-    samtools -@24 fastq --reference "$reference" -1 "$output_folder/${sample_name}_out.R1.fastq" -2 "$output_folder/${sample_name}_out.R2.fastq" "$cram_file"
-    
-    echo "Conversion completed for $cram_file"
+    sbatch <<EOF
+#!/bin/bash
+#SBATCH --job-name=cram2fq_${sample_name}
+#SBATCH --output=cram2fq/cram2fq_${sample_name}.out
+#SBATCH --error=cram2fq/cram2fq_${sample_name}.err
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=24
+
+# Run samtools fastq for each CRAM file
+echo "Conversion started"
+samtools fastq -@24 -t --reference "$reference" -1 "$output_folder/${sample_name}.R1.fastq" -2 "$output_folder/${sample_name}.R2.fastq" "$cram_file"
+
+echo "Conversion completed for $cram_file"
+
+# Zip the produced FASTQ files
+gzip "$output_folder/${sample_name}.R1.fastq" & 
+gzip "$output_folder/${sample_name}.R2.fastq" &
+
+wait
+
+echo "Conversion and compression completed for $cram_file"
+
+EOF
 done
