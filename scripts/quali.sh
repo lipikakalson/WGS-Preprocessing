9ca0497c01716f211a5bd504020b07cfb00139cc
lipika.kalson <84702870+lipikakalson@users.noreply.github.com>
2025-04-10 14:43:26 +0200

    Update quali.sh

--- scripts/quali.sh
+++ scripts/quali.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 #SBATCH --job-name='qualimap'
-#SBATCH --mem=256GB
+#SBATCH --mem=30GB
 #SBATCH --mail-user=lipika.lipika@medunigraz.at
 #SBATCH --partition=cpu
-#SBATCH --cpus-per-task=36
-#SBATCH --output=quali-output.o
-#SBATCH --error=quali-error.e
+#SBATCH --output=quali-output/quali-output.o
+#SBATCH --error=quali-output/quali-error.e
 squeue -u 'o_lipika'
 
 ulimit -c unlimited
+unset DISPLAY
 
 #ref="/home/isilon/users/o_lipika/FFPE-WGS-samples/refernces/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa"
 #dbsnp="/home/isilon/users/o_lipika/FFPE-WGS-samples/refernces/ref/gatk/dbsnp_146.hg38.vcf.gz"
 #indel="/home/isilon/users/o_lipika/FFPE-WGS-samples/refernces/ref/gatk/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
 
-INPUT_DIR="/home/isilon/patho_anemone-meso/updated-data-bam/fastq/trimmed/batch2"
-ouput="/home/isilon/patho_anemone-meso/updated-data-bam/fastq/trimmed/qualimap"
-ouput_dir1="/home/isilon/patho_anemone-meso/updated-data-bam/fastq/trimmed/flagstat_results"
+INPUT_DIR="/home/isilon/patho_anemone-meso/fastq/dedup/samtools/"
+ouput="/home/isilon/patho_anemone-meso/fastq/qc-files/qualimap"
+ouput_dir1="/home/isilon/patho_anemone-meso/fastq/qc-files/flagstat_results"
 
 
-for bam_file in "$INPUT_DIR"/*dedup_output_file_BQSRecalibrated.bam; do
+for bam_file in "$INPUT_DIR"/*BQSRecalibrated.bam; do
   echo "bam file: $bam_file"
   # Extract filename without path and directory part
   filename=$(basename "$bam_file")
@@ -29,17 +29,17 @@ for bam_file in "$INPUT_DIR"/*dedup_output_file_BQSRecalibrated.bam; do
  sbatch <<EOF
 #!/bin/bash
 #SBATCH --job-name=quali_${filename%.*}
-#SBATCH --output=quali_${filename%.*}.out
-#SBATCH --error=quali_${filename%.*}.err
+#SBATCH --output=quali-output/quali_${filename%.*}.out
+#SBATCH --error=quali-output/quali_${filename%.*}.err
 #SBATCH --partition=cpu
-#SBATCH --cpus-per-task=36
-#SBATCH --mem=256GB
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=50G
 
+#echo "quali submit"
+qualimap bamqc -nt 4 --skip-duplicated -bam $bam_file -outdir $ouput/$filename -outformat html -c --java-mem-size=40G 
+#echo "qualimap done and sambamba submit "
+sambamba flagstat -t 4 $bam_file  > ${ouput_dir1}/${filename}.stats.txt 
 
-echo "quali submit"
-qualimap bamqc -nt 36 --skip-duplicated -bam $bam_file --java-mem-size=256G -outdir $ouput/$filename -outformat html
-echo "qualimap done and sambamba submitted"
-sambamba flagstat -t 36 $bam_file  > ${ouput_dir1}/${filename}.stats.txt
 echo "sambamba done"
 
 EOF
