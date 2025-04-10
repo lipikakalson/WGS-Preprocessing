5902785c4878db35758cd12ad00c83d26647501f
lipika.kalson <84702870+lipikakalson@users.noreply.github.com>
2025-04-10 14:34:09 +0200

    Update mergeBAM-2.sh

--- scripts/mergeBAM-2.sh
+++ scripts/mergeBAM-2.sh
@@ -1,42 +1,35 @@
 #!/bin/sh
-#SBATCH --job-name='merge'
-#SBATCH --mem=256GB
+#SBATCH --job-name='merge2'
 #SBATCH --mail-user=lipika.lipika@medunigraz.at
 #SBATCH --partition=cpu
-#SBATCH --output=merge/output-merge.o
-#SBATCH --error=merge/error-merge.e
+#SBATCH --output=merge/output-merge2.o
+#SBATCH --error=merge/error-merge2.e
 
 squeue -u 'o_lipika'
 
 fastq_dir="/home/isilon/patho_anemone-meso/fastq/trimmed"
 
-# Set the reference genome file
-reference_genome="/home/isilon/users/o_lipika/FFPE-WGS-samples/refernces/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa"
-alt_file="/home/isilon/users/o_lipika/FFPE-WGS-samples/refernces/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa.alt"
-
-# Align reads to the new reference genome
 for file1 in $fastq_dir/*lane.txt; do
-    
-    filename1="${file1##*/}"
-    #echo "filename1" $filename1
 
-    sample_name=$(echo "$filename1" | sed 's/_lane.txt//')
-    #sample_name=$(echo "${filename1%_sorted*}" | sed 's/\.bam$//')
-    echo "sample_name" $sample_name
+    # Extract first BAM file from list
+    first_bam=$(head -n 1 "$file1")
 
-    #rg_file="$fastq_dir/${sample_name}_RG.txt"
-    #sample_file="$fastq_dir/${sample_name}_lane.txt"
+    # Extract SM tag from @RG line in BAM header
+    SM=$(samtools view -H "$first_bam" | grep "^@RG" | head -n 1 | sed -n 's/.*SM:\([^ \t]*\).*/\1/p')
 
+    echo "Merging for sample: $SM"
 
     sbatch <<EOF
 #!/bin/bash
-#SBATCH --job-name=merge_${sample_name##*/}
-#SBATCH --output=merge/merge_${sample_name##*/}.out
-#SBATCH --error=merge/merge_${sample_name##*/}.err
+#SBATCH --job-name=merge_${SM}
+#SBATCH --output=merge/merge_${SM}.out
+#SBATCH --error=merge/merge_${SM}.err
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=12
+#SBATCH --mem=96GB
 
-samtools merge -@12 -r $fastq_dir/${sample_name}_merged.bam -b $file1 
-
+samtools merge -@12 -r $fastq_dir/${SM}_merged.bam -b $file1
+echo "Merged BAM created: ${SM}_merged.bam"
 EOF
+
 done
